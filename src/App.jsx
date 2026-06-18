f5f9bd9383727ade4a92900c776b8cc1ff93bd89
mmpax01 <261549090+mmpax01@users.noreply.github.com>
2026-06-19 06:23:13 +1200

    Create page JobsPage.jsx. Refactor Navbar and all buttons using a-tag and replace with Link tags.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import {
   Route, createBrowserRouter, createRoutesFromElements, RouterProvider
 } from 'react-router-dom';
-import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/HomePage';
+import JobsPage from './pages/JobsPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
     </Route>
   )
 );
