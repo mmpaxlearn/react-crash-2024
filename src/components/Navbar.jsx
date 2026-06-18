d7e67aef8e7409c6bae90beb0554852d0836d17d
mmpax01 <261549090+mmpax01@users.noreply.github.com>
2026-06-19 05:58:07 +1200

    Refactor Navbar href's .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/images/logo.png'
 const Navbar = () => {
   return (
     <>
-    <nav className="bg-indigo-700 border-b border-indigo-500">
+      <nav className="bg-indigo-700 border-b border-indigo-500">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="flex h-20 items-center justify-between">
             <div
@@ -23,17 +23,17 @@ const Navbar = () => {
               <div className="md:ml-auto">
                 <div className="flex space-x-2">
                   <a
-                    href="/index.html"
+                    href="/index"
                     className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                   >Home</a
                   >
                   <a
-                    href="/jobs.html"
+                    href="/jobs"
                     className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                   >Jobs</a
                   >
                   <a
-                    href="/add-job.html"
+                    href="/add-job"
                     className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                   >Add Job</a
                   >
