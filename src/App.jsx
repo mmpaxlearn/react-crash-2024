e5fef6ae2dea9e83c5437502a7c46d8bffa83935
mmpax01 <261549090+mmpax01@users.noreply.github.com>
2026-06-18 22:47:31 +1200

    Import functions from react-router-dom for initial setup.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
-import React from 'react'
+import {
+  Route, createBrowserRouter, createRoutesFromElements, RouterProvider
+} from 'react-router-dom';
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import HomeCards from './components/HomeCards'
 import JobListings from './components/JobListings'
 import ViewAllJobs from './components/ViewAllJobs'
 
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<h1>My App</h1>} />)
+);
+
 const App = () => {
-  return (
-    <>
-      <Navbar />
+  return <RouterProvider router={router} />;
+  
+};
 
-      <Hero title="Become a React Dev" subtitle="Find the React job that fits your skills and needs" />
-
-      <HomeCards />
-
-      <JobListings />
-
-      <ViewAllJobs />
-    </>
-  )
-}
-
-export default App
+export default App;
