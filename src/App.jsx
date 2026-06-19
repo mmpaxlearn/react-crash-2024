48394fdec99b7e177f493880b8fc555d75cebc4a
mmpax01 <261549090+mmpax01@users.noreply.github.com>
2026-06-19 23:03:59 +1200

    feat - add JobPage component. Update App.js with JobPage-Route using dynamic path parameter (:id) for job-id.

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
+import JobPage from './pages/JobPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path='/jobs' element={<JobsPage />} />
+      <Route path='/jobs/:id' element={<JobPage />} /> // :id is a URL parameter that is a dynamic route
       <Route path='*' element={<NotFoundPage />} /> // * is a Catch-all route for 404 Not Found
     </Route>
   )
