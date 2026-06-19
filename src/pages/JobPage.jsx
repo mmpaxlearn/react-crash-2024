2b85df0d32ef570eaa1ad99087d2341f41eda985
mmpax01 <261549090+mmpax01@users.noreply.github.com>
2026-06-19 23:53:19 +1200

    feat: implement data-fetching function in JobPage.

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,7 +1,31 @@
-
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import Spinner from '../components/Spinner';
 
 const JobPage = () => {
-  return <div>JobPage</div>;
+  const { id } = useParams(); // useParams is a hook that allows us to access the URL parameters
+  const [job, setJob] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+
+    const fetchJob = async () => {
+      try {
+        const res = await fetch(`/api/jobs/${id}`);
+        const data = await res.json();
+        console.log(data);
+        setJob(data);
+      } catch (error) {
+        console.log('Error fetching data:', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchJob();
+  }, []);
+
+  return loading ? <Spinner /> : <h1>{job?.title}</h1>
 };
 
 export default JobPage;
